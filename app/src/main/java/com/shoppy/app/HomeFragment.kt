98a196e777f09f54e626df0a0324a0635ab5762d
Jammini <systemdata227@gmail.com>
2022-05-11 15:50:47 +0900

    홈화면 배너 Adapter 구현 및 데이터 전달

--- app/src/main/java/com/shoppy/app/HomeFragment.kt
+++ app/src/main/java/com/shoppy/app/HomeFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import org.json.JSONObject
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -24,22 +26,54 @@ class HomeFragment : Fragment() {
 
         val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
         val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
-
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
-        Log.d("homeData", homeData ?: "")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeJsonString ?: "")
 
-        if (!homeData.isNullOrEmpty()) {
-            val jsonObject = JSONObject(homeData)
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
+
+/*
+            val jsonObject = JSONObject(homeJsonString)
             val title = jsonObject.getJSONObject("title")
             val text = title.getString("text")
             val iconUrl = title.getString("icon_url")
             val titleValue = Title(text, iconUrl)
-            toolbarTitle.text = titleValue.text
+*/
+
+            toolbarTitle.text = homeData.title.text
             GlideApp.with(this)
-                .load(titleValue.iconUrl)
+                .load(homeData.title.iconUrl)
                 .into(toolbarIcon)
+
+
+/*            val topBanners = jsonObject.getJSONArray("top_banners")
+            val size = topBanners.length()
+
+            for (index in 0 until size) {
+                val bannerObject = topBanners.getJSONObject(index)
+                val backgroundImageUrl = bannerObject.getString("background_image_url")
+                val badgeObject = bannerObject.getJSONObject("badge")
+                val badgeLabel = badgeObject.getString("label")
+                val badgeBackgroundColor = badgeObject.getString("background_color")
+                val bannerBadge = BannerBadge(badgeLabel, badgeBackgroundColor)
+                val banner = Banner(
+                    backgroundImageUrl,
+                    bannerBadge,
+                    bannerLabel,
+                    bannerProductDetail
+                )
+            }*/
+
+
+            viewpager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanner)
+
+            }
         }
     }
 }
