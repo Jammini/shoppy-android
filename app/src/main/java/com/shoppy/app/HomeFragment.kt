d2fc8e776cf10ab205ad9c9dbf5d98a401364f9e
Jammini <systemdata227@gmail.com>
2022-05-13 11:22:28 +0900

    홈화면 배너 페이지 위치 조정 및 Indicator 구현

--- app/src/main/java/com/shoppy/app/HomeFragment.kt
+++ app/src/main/java/com/shoppy/app/HomeFragment.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
@@ -74,6 +75,19 @@ class HomeFragment : Fragment() {
                 submitList(homeData.topBanner)
 
             }
+
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWith = resources.displayMetrics.widthPixels
+            val offset = screenWith - pageWidth - pageMargin
+
+            viewpager.offscreenPageLimit = 3
+            viewpager.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            TabLayoutMediator(viewpagerIndicator, viewpager) { tab, position ->
+
+            }.attach()
         }
     }
 }
